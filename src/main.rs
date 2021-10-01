1b9f15c17c95f189edf1ace6d638a841e7acf320
Rick Shiguemoto <azrick88@outlook.com>
2021-10-01 08:46:26 -0300

    função Lexer foi simplificada e os erros foram eliminados
    Agora lexer retorna um vetor de strings e não passamos o retorno como parâmetro

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 #![allow(non_snake_case)] // Deixar a gente não usar underline _
 
-pub fn lexer<'a>(expressao: String, retorno: &'a mut Vec<String>) -> &'a Vec<String>{
+pub fn lexer<'a>(expressao: String) -> Vec<String>{
+    let simbolos_separados = expressao.chars(); // Vetor de caracteres que possui todos os símbolos de "expressao" separados
+    let mut aux_retorno : Vec<String> = Vec::new(); // Vetor de string que terá os valores de simbolos_separados diferentes de ' ' 
+                                                    // convertidos em string
+    let mut retorno : Vec<String> = Vec::new(); // Vetor de strings que será retornada
     
-    let mut aux = expressao.chars();
-    let mut aux_retorno : Vec<String> = Vec::new(); 
     
-    for character in aux{
+    //Passamos por simbolos_separados e pegamos só os simbolos diferentes de ' ' e colocamos em retorno
+    for character in simbolos_separados{
         if character != ' ' {
-            retorno.push(character.to_string());
+            aux_retorno.push(character.to_string());
         }
     }
 
-    //função que junta numeros em sequencia
-    for palavra in retorno{
-        if (palavra != "*") && (palavra != "/") && (palavra != "(") && (palavra != ")") && (palavra != "+") && (palavra != "-"){
-            aux_retorno.push(palavra.to_string());
-        }
-    }
+    //Precisamos juntar os números de aux_retorno
 
-
-    &aux_retorno
+    return retorno;
 }
 
 fn main() {
-    let expressaoMath = String::from("31 * (4 + 10)");
-    let retorno: Vec<String> = Vec::new();
+    let expressaoMath = Box::new(String::from("31 * (4 + 10)"));    
 }
